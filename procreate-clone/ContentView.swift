87472a7d1e60f91e191a5c295f16ca61143a6f8e
Lescai, Ionel <ionel.lescai@flir.com>
2021-06-28 11:37:24 +0300

    Added SwiftUI project

---
+++ procreate-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  procreate-clone
+//
+//  Created by Lescai, Ionel on 2021-06-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ procreate-clone/procreate_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  procreate_cloneApp.swift
+//  procreate-clone
+//
+//  Created by Lescai, Ionel on 2021-06-28.
+//
+
+import SwiftUI
+
+@main
+struct procreate_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
