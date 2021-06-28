982778849880ad1605d1ab90fb23ea578534a3f6
Anuta Cosmin <cosmin.anuta@flir.com>
2021-06-28 13:42:13 +0300

    Add scroll to reveal DC under IR

--- procreate-clone/ThermalImageView.swift
+++ procreate-clone/ThermalImageView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ThermalImageView: View {
+    @State
+    private var revealValue: CGFloat = 0.0
+    
     var body: some View {
-        Image("copacIR")
-            .resizable()
+        
+        GeometryReader { geo in
+            ZStack {
+                Image("copacDC")
+                    .resizable()
+                Image("copacIR")
+                    .resizable()
+                    .mask(
+                        HStack {
+                            Rectangle()
+                                .frame(width: revealValue * geo.size.width)
+                            Spacer()
+                        }
+                    )
+            }
+            .gesture(DragGesture()
+                        .onChanged { value in
+                            revealValue = max(0, min(1, (value.startLocation.x + value.translation.width) / geo.size.width))
+                        })
+        }
     }
 }
 
