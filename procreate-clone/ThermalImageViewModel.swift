29eafe33a79020f6af80bd1baae85ae40eda8252
Anuta Cosmin <cosmin.anuta@flir.com>
2021-08-30 13:13:41 +0300

    Add scale data in thermal image view model

--- procreate-clone/ThermalImageViewModel.swift
+++ procreate-clone/ThermalImageViewModel.swift
@@ -12,22 +12,32 @@ import SwiftUI
 class ThermalImageViewModel: ObservableObject {
     private let thermalImage = FLIRThermalImageFile()
     
-    @Published var dcImage: UIImage
-    @Published var irImage: UIImage
+    @Published var dcImage: UIImage?
+    @Published var irImage: UIImage?
+    @Published var scaleImage: UIImage?
     
     init(path: String) {
         thermalImage.open(path)
         thermalImage.getImage()
-        thermalImage.getFusion()?.setFusionMode(FUSION_MSX_MODE)
-        irImage = thermalImage.getImage()!
-        thermalImage.getFusion()?.setFusionMode(VISUAL_MODE)
-        dcImage = thermalImage.getImage()!
+        super.init()
+        updateImages()
     }
     
     private func updateImages() {
         thermalImage.getFusion()?.setFusionMode(FUSION_MSX_MODE)
         irImage = thermalImage.getImage()!
+        scaleImage = thermalImage.getScale()!.getImage()!
         thermalImage.getFusion()?.setFusionMode(VISUAL_MODE)
         dcImage = thermalImage.getImage()!
+    }
+    
+    func setScaleMin(value: Double) {
+        thermalImage.getScale()!.setRangeMin(FLIRThermalValue(value: value, andUnit: .CELSIUS))
+        updateImages()
+    }
+    
+    func setScaleMax(value: Double) {
+        thermalImage.getScale()!.setRangeMax(FLIRThermalValue(value: value, andUnit: .CELSIUS))
+        updateImages()
     }
 }
